0aa7df60ef33ae272a5cb82ff1b1abeb94a0134f
Iven <iwenchak@gmail.com>
2020-06-25 19:49:51 +0300

    win forms

--- surface/Start.Designer.cs
+++ surface/Start.Designer.cs
@@ -71,7 +71,7 @@
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(134, 48);
 			this.button1.TabIndex = 2;
-			this.button1.Text = "Submit";
+			this.button1.Text = "Calculation";
 			this.button1.UseVisualStyleBackColor = true;
 			this.button1.Click += new System.EventHandler(this.button1_Click);
 			// 
